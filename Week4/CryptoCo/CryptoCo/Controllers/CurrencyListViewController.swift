6ee4debe771698cc0e1555b5b5c2960565c27895
Alihan KUZUCUK <95879997+alihankuzucuk@users.noreply.github.com>
2022-11-21 20:43:08 +0300

    ActivityIndicator added to the CurrencyListViewController using MaterialActivityIndicator library

--- Week4/CryptoCo/CryptoCo/Controllers/CurrencyListViewController.swift
+++ Week4/CryptoCo/CryptoCo/Controllers/CurrencyListViewController.swift
@@ -6,33 +6,54 @@
 //
 
 import UIKit
+import MaterialActivityIndicator
 
 final class CurrencyListViewController: UIViewController {
 
+    // MARK: - Outlets
     @IBOutlet private weak var tableViewAssets: UITableView!
     
+    // MARK: - Properties
     private var assets: [AssetModel]? {
         didSet {
             tableViewAssets.reloadData()
         }
     }
     
+    let indicator = MaterialActivityIndicatorView()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        setupActivityIndicatorView()
+        indicator.startAnimating()
         Client.getAssets { [weak self] assets, error in
             /**
              [weak self] is a capture list here
              We added this capture list so that self reference does not prevent the clearing memory at any time
              */
             guard let self = self else { return }
+            self.indicator.stopAnimating()
             self.assets = assets
         }
     }
     
+    // MARK: - Methods
     private func configureTableView() {
         tableViewAssets.dataSource = self
         tableViewAssets.register(UINib(nibName: "CurrencyTableViewCell", bundle: nil), forCellReuseIdentifier: "CurrencyCell")
+    }
+    
+    private func setupActivityIndicatorView() {
+        view.addSubview(indicator)
+        setupActivityIndicatorViewConstraints()
+    }
+    
+    private func setupActivityIndicatorViewConstraints() {
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        indicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 }
