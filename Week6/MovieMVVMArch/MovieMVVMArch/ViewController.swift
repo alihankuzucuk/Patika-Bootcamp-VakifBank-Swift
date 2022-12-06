b676b58192f102e0715948515012ce7f81e4390c
Alihan KUZUCUK <95879997+alihankuzucuk@users.noreply.github.com>
2022-12-06 10:46:21 +0300

    The project Week6/MovieMVVMArch has been created

---
+++ Week6/MovieMVVMArch/MovieMVVMArch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMVVMArch
+//
+//  Created by Alihan KUZUCUK on 6.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
